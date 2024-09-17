2dbedad14691f60d6aa816a72c04ed88b88c56fb
Alexandre BOURGUIGNON <brgn.alexandre@gmail.com>
2024-09-18 00:55:24 +0200

    Delete useless files

--- src/config/app_config.js
+++
@@ -1,3 +0,0 @@
-export class AppConfig {
-    static API_URL = 'http://172.21.5.11:8080/api/v1/';
-}
--- src/services/auth-header.js
+++
@@ -1,9 +0,0 @@
-export default function authHeader() {
-    let user = JSON.parse(localStorage.getItem('user'));
-    // console.log('AuthHeader - token() :', user.success.token);
-    if (user && user.success.token) {
-        return { Authorization: 'Bearer ' + user.success.token };
-    } else {
-        return {};
-    }
-}
